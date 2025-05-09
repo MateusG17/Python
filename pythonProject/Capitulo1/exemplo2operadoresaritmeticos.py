1e340370a2e3e523d19db9ac26340fccc99b7240
isadora.fiori <Andrey.schmitz@escola.pr.gov.br>
2025-05-09 11:42:33 -0300

    exercicio 1

--- pythonProject/Capitulo1/exemplo2operadoresaritmeticos.py
+++ pythonProject/Capitulo1/exemplo2operadoresaritmeticos.py
@@ -2,4 +2,4 @@ soma = 40 + 2
 subtracao = 43 - 1
 multiplicacao = 6 * 7
 divisao = 84 / 2
-exponenciacao = 2 ** 3 #simplifica 2 elevado a 3
+exponenciacao = 2 ** 3 #significa 2 elevado a 3
---
+++ pythonProject/Capitulo2/exercicio1.py
@@ -0,0 +1,19 @@
+num1 = 4
+num2 = 3
+num3 = 3.14
+num4 = 5
+num5 = 3
+
+total = (num1 / num2) * num3 * num4 ** num5
+
+print(total)
+
+#Outra Forma de Fazer
+
+#primeiroTermo = 4/3
+#pi = 3.14
+#raio = 5**3
+
+#total = primeiroTermo * pi * raio
+
+#print('o volume de uma esfera de raio 5 e de', total)
