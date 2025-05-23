981ea065a0f0999dd22689cc6e1700dd6870deff
mateus <mateus.gomes.dias@escola.pr.gov.br>
2025-05-23 11:14:15 -0300

    Exercicio 2 Lista de exercicio

--- pythonProject/Capitulo2/Lista/Exercicio 2.py
+++ pythonProject/Capitulo2/Lista/Exercicio 2.py
@@ -1,19 +1,15 @@
 # Aplicativo para separar dígitos de um número 5 dígitos
 
 # Solicita o número as usuário
-num = int(input("Digite um número de 5 dígitos "))
+numero = int(input("Digite um número de 5 dígitos: "))
+digito1 = numero // 10000
+digito2 = (numero % 10000) // 1000
+digito3 = (numero % 1000) // 100
+digito4 = (numero % 100) // 10
+digito5 = numero % 10
 
-# Extrai cada dígito usando operações matemáticas
-    num1 = num // 10000
-    num2 = (num % 10000) // 1000
-    num3 = (num % 1000) // 100
-    num4 = (num % 100) // 10
-    num5 = num % 10
-
- # Imprime os dígitos um em baixo do outro
-print(" ", num1)
-print(" ", num2)
-print(" ", num3)
-print(" ", num4)
-print(" ", num5)
-
+print(digito1," ")
+print(digito2," ")
+print(digito3," ")
+print(digito4," ")
+print(digito5," ")
