e3ea638b59306308ebfd191b6b24fc6a2794342f
mateus <mateus.gomes.dias@escola.pr.gov.br>
2025-05-23 11:06:41 -0300

    Exercicio 2 Lista de exercicio Certa

--- pythonProject/Capitulo2/Lista/Exercicio 2.py
+++ pythonProject/Capitulo2/Lista/Exercicio 2.py
@@ -3,16 +3,17 @@
 # Solicita o número as usuário
 num = int(input("Digite um número de 5 dígitos "))
 
-# Verifica se o número tem 5 dìgitos
-if 10000 <= num <= 99999:
-    # Extrai cada dígito usando operações matemáticas
-    digito1 = num // 10000
-    digito2 = (num % 10000) // 1000
-    digito3 = (num % 1000) // 100
-    digito4 = (num % 100) // 10
-    digito5 = num % 10
+# Extrai cada dígito usando operações matemáticas
+    num1 = num // 10000
+    num2 = (num % 10000) // 1000
+    num3 = (num % 1000) // 100
+    num4 = (num % 100) // 10
+    num5 = num % 10
 
-    # Imprime os dígitos separados por três espaços
-    print(f"{digito1} {digito2} {digito3} {digito4} {digito5}")
-else:
-    print("Por favor, digite um número de exatamente 5 dígitos")
+ # Imprime os dígitos um em baixo do outro
+print(" ", num1)
+print(" ", num2)
+print(" ", num3)
+print(" ", num4)
+print(" ", num5)
+
